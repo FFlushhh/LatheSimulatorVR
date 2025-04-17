6d8d5d73cb4e0dbb2d34b3b6aa75e71f55ce0517
FFlush <kyaramel.022626@gmail.com>
2025-04-17 13:16:34 +0900

    プロジェクト作成

--- Assets/Script/SquareTablePosSet.cs
+++ Assets/Script/SquareTablePosSet.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-//using Meta.WitAi.Utilities;
+// using Meta.WitAi.Utilities;
 using Unity.VisualScripting;
 using UnityEngine;
 
